2403162acaf86ce46018372e19e4c6277976582c
Brosup-dev <locnc.brosup@gmail.com>
2025-07-16 10:04:44 +0700

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://stalwart-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-      }
-    }
-  }
+  base: '/',
 })
